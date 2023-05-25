b23f1c7c1e0f847c27534be91e46e9fb2437d248
irajesicajanuaris <jjiraaa1@gmail.com>
2023-05-25 17:00:34 +0700

    menambahkan fungsi searchData() di class DoubleLinkedList

--- double_linked_list1/double_linked_list1.cpp
+++ double_linked_list1/double_linked_list1.cpp
@@ -142,5 +142,23 @@ void DoubleLinkedList::hapus() {
 	if (DoubleLinkedList::deleteNode(rollNo) == false)
 		cout << "Record not found" << endl;
 	else
-		cout << "Record with roll number " << rollNo << "deleted" << endl; 
+		cout << "Record with roll number " << rollNo << "deleted" << endl;
+}
+void DoubleLinkedList::searchData() {
+	if (listEmpty() == true) {
+		cout << "\nList is empty" << endl;
+
+	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whose record you want to search: ";
+	int num;
+	cin >> num;
+	if (DoubleLinkedList::search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else {
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number: " << curr->noMhs << endl;
+		cout << "\nName: " << curr->name << endl;
+	}
 }
