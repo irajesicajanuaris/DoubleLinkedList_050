aced77e85d32e0b315e921c70bef43b907ea9089
irajesicajanuaris <jjiraaa1@gmail.com>
2023-05-25 11:18:55 +0700

    menghubungkan newNode dengan benar di DoubleLinkedList

--- double_linked_list1/double_linked_list1.cpp
+++ double_linked_list1/double_linked_list1.cpp
@@ -59,7 +59,15 @@ void DoubleLinkedList::addNode() {
 	while (current->next != NULL && nim > current->next->noMhs)
 		current = current->next;
 
-	if (current->next != NULL && nim == current->next->noMhs)
+	if (current->next != NULL && nim == current->next->noMhs) {
 		cout << "\nDuplicate roll numbers not allowed" << endl;
-	return;
+		return;
+	}
+
+	newNode->next = current->next;
+	newNode->prev = current;
+	if (current->next != NULL)
+		current->next->prev = newNode;
+	current->next = newNode;
 }
+
