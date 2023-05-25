1a8568693f13313d93c15771188d5322593eba4c
irajesicajanuaris <jjiraaa1@gmail.com>
2023-05-25 12:09:00 +0700

    menambahkan fungsi hapus() untuk memimnta pengguna memasukkan rollNo dan menghapus relaman terkait dari Doublelinkedlist

--- double_linked_list1/double_linked_list1.cpp
+++ double_linked_list1/double_linked_list1.cpp
@@ -128,6 +128,19 @@ void DoubleLinkedList::descending() {
 			currentNode = currentNode->prev;
 		}
 	}
-
 }
 
+void DoubleLinkedList::hapus() {
+	if (listEmpty()) {
+		cout << "\nList is Empty" << endl;
+
+	}
+	cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+	int rollNo;
+	cin >> rollNo;
+	cout << endl;
+	if (DoubleLinkedList::deleteNode(rollNo) == false)
+		cout << "Record not found" << endl;
+	else
+		cout << "Record with roll number " << rollNo << "deleted" << endl; 
+}
